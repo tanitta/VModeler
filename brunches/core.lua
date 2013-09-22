0173f14ca53524c05470702e2fd01676c29df720
tanitta <ttata.trit@gmail.com>
2013-09-22 17:13:09 +0900

    入出力クラスひな形だけ追加
    Vmodelのメソッドとして入出力を実装したほうがいいかも

--- brunches/core.lua
+++ brunches/core.lua
@@ -1,5 +1,31 @@
 namespace"trit"{
 	namespace"VModeler"{
+		class"Vmodel"{
+		    	metamethod"_init"
+		    	:body(function(self)
+		    	end);
 		
+		    	metamethod"__call"
+		    	:body(function(self)
+		    	end);
+		};
+		class"VImport"{
+		    	metamethod"_init"
+		    	:body(function(self)
+		    	end);
+		
+		    	metamethod"__call"
+		    	:body(function(self)
+		    	end);
+		};
+		class"VExport"{
+		    	metamethod"_init"
+		    	:body(function(self)
+		    	end);
+		
+		    	metamethod"__call"
+		    	:body(function(self)
+		    	end);
+		};
 	}
 }
