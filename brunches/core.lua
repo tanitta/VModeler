8e1b15c2c72c9e3c18162efcb232b31b3cfc74da
tanitta <ttata.trit@gmail.com>
2013-09-25 00:35:02 +0900

    Bodyブロックの解析実装開始
    #RigidChips

--- brunches/core.lua
+++ brunches/core.lua
@@ -15,6 +15,7 @@ namespace"trit"{
 		    			}}
 		    		}
 		    		self.tabLua = {}
+		    		
 		    	end);
 		
 		    	metamethod"__call"
@@ -40,84 +41,92 @@ namespace"trit"{
 		    		self.strRaw = self.objTxtFile:GetString()
 					
 				end);
+				method"AnalyzeValueBlock"
+				:body(function(self)
+		    		-- h, f = string.find(self.strRaw,"Val")
+		    		local h, f = string.find(self.strRaw,"{.-}")
+		    		h = h + 2
+		    		f = f - 1
+	    			local strVal = string.sub(self.strRaw,h,f)
+	    			local strValL = ""
+	    			local i = 0
+		    		repeat 
+		    			
+		    			h, f = string.find(strVal,"\n")
+		    			local lEnd = f
+						strValL = string.sub(strVal,1,lEnd)
+		    			
+		    			if strValL ~= "\t\n" then
+							i = i + 1
+			    			self.tabVal[i] = {}
+			    			
+			    			-- name
+		    				h, f = string.find(strValL,"[%w-_]-%(")
+			    			self.tabVal[i].name = string.sub(strValL,h,f-1)
+			    			
+			    			--default
+		    				h, f = string.find(strValL,"default=%-?%d-.?%d-[%,%)]")
+		    				if h == nil then
+		    					self.tabVal[i].default = 0
+		    				else
+			    				self.tabVal[i].default = tonumber(string.sub(strValL,h+8,f-1))	
+		    				end
+			    			
+			    			--min
+		    				h, f = string.find(strValL,"min=%-?%d-.?%d-[%,%)]")
+		    				if h == nil then
+		    					self.tabVal[i].min = -9999999999999999
+		    				else
+			    				self.tabVal[i].min = tonumber(string.sub(strValL,h+4,f-1))
+		    				end
+			    			
+			    			--max
+		    				h, f = string.find(strValL,"max=%-?%d-.?%d-[%,%)]")
+		    				if h == nil then
+		    					self.tabVal[i].max = 9999999999999999
+		    				else
+			    				self.tabVal[i].max = tonumber(string.sub(strValL,h+4,f-1))
+		    				end
+			    			
+			    			--step
+		    				h, f = string.find(strValL,"step=%-?%d-.?%d-[%,%)]")
+		    				if h == nil then
+		    					self.tabVal[i].step = 0
+		    				else
+			    				self.tabVal[i].step = tonumber(string.sub(strValL,h+5,f-1))
+		    				end
+			    			
+			    			--disp
+		    				h, f = string.find(strValL,"disp=%-?%d-.?%d-[%,%)]")
+		    				if h == nil then
+		    					self.tabVal[i].disp = 0
+		    				else
+			    				self.tabVal[i].disp = tonumber(string.sub(strValL,h+5,f-1))
+		    				end
+			    			
+		    			end
+		    			
+		    			strVal = string.sub(strVal,lEnd+1)
+		    			
+		    		until strVal == ""
+			    
+				end);
 		
+				method"AnalyzeBodyBlock"
+				:body(function(self)
+					local h,f = 0, 0
+					local depth = 0
+					h,f = string.find(self.strRaw,"Body\n{.*}\nLua")
+					local strBody = string.sub(self.strRaw,h+7,f-5)
+					-- out(0,strBody)
+					local block = function() end
+				end);
+			
 		    	method"GenerateData"
 		    	:body(function(self)
-		    		local h,f = 0,0
-		    		local strBuffer = ""
+		    		self:AnalyzeValueBlock()
+		    		self:AnalyzeBodyBlock()
 		    		
-		    		-- ValBlock
-		    		do
-			    		-- h, f = string.find(self.strRaw,"Val")
-			    		h, f = string.find(self.strRaw,"{.-}")
-			    		h = h + 2
-			    		f = f - 1
-		    			local strVal = string.sub(self.strRaw,h,f)
-		    			local strValL = ""
-		    			local i = 0
-			    		repeat 
-			    			
-			    			h, f = string.find(strVal,"\n")
-			    			local lEnd = f
-							strValL = string.sub(strVal,1,lEnd)
-			    			
-			    			if strValL ~= "\t\n" then
-								i = i + 1
-				    			self.tabVal[i] = {}
-				    			
-				    			-- name
-			    				h, f = string.find(strValL,"[%w-_]-%(")
-				    			self.tabVal[i].name = string.sub(strValL,h,f-1)
-				    			
-				    			--default
-			    				h, f = string.find(strValL,"default=%-?%d-.?%d-[%,%)]")
-			    				if h == nil then
-			    					self.tabVal[i].default = 0
-			    				else
-				    				self.tabVal[i].default = tonumber(string.sub(strValL,h+8,f-1))	
-			    				end
-				    			
-				    			--min
-			    				h, f = string.find(strValL,"min=%-?%d-.?%d-[%,%)]")
-			    				if h == nil then
-			    					self.tabVal[i].min = -9999999999999999
-			    				else
-				    				self.tabVal[i].min = tonumber(string.sub(strValL,h+4,f-1))
-			    				end
-				    			
-				    			--max
-			    				h, f = string.find(strValL,"max=%-?%d-.?%d-[%,%)]")
-			    				if h == nil then
-			    					self.tabVal[i].max = 9999999999999999
-			    				else
-				    				self.tabVal[i].max = tonumber(string.sub(strValL,h+4,f-1))
-			    				end
-				    			
-				    			--step
-			    				h, f = string.find(strValL,"step=%-?%d-.?%d-[%,%)]")
-			    				if h == nil then
-			    					self.tabVal[i].step = 0
-			    				else
-				    				self.tabVal[i].step = tonumber(string.sub(strValL,h+5,f-1))
-			    				end
-				    			
-				    			--disp
-			    				h, f = string.find(strValL,"disp=%-?%d-.?%d-[%,%)]")
-			    				if h == nil then
-			    					self.tabVal[i].disp = 0
-			    				else
-				    				self.tabVal[i].disp = tonumber(string.sub(strValL,h+5,f-1))
-			    				end
-				    			
-				    			out(i,self.tabVal[i].disp)
-			    			end
-			    			
-			    			strVal = string.sub(strVal,lEnd+1)
-			    			
-			    		until strVal == ""
-			    	end	
-			    	-- BodyBlock
-			    	-- LuaBlock
 		    	end);
 		};
 		
